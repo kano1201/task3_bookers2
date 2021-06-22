406b95e4971feb356f7a3d4396d325cfe19fc24d
kano1201 <kano.nakamura3@gmail.com>
2021-06-22 05:39:02 +0000

    book new,index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,17 @@
 class BooksController < ApplicationController
   def new
+     @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -15,9 +21,15 @@ class BooksController < ApplicationController
   end
   
   def update
-    
+  end
 
   def destroy
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
