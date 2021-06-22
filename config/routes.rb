ee36f10b0826e2a61efc3e9dfdb54153eff24369
kano1201 <kano.nakamura3@gmail.com>
2021-06-22 07:02:47 +0000

    userinfo User show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
-   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show]
 end
