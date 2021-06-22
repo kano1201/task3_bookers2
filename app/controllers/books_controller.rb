01197f92d4a371a03fc01c001b60dbb15a976efa
kano1201 <kano.nakamura3@gmail.com>
2021-06-22 08:46:36 +0000

    edit error update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -23,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path()
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
+    
   end
 
   def destroy
