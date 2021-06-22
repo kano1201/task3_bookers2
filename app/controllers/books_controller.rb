984b1c3b4a3d4f862a50554a65beb8dc11bfcca1
kano1201 <kano.nakamura3@gmail.com>
2021-06-22 09:27:59 +0000

    book create.update サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,8 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       render :new
     end
@@ -28,11 +29,12 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
      redirect_to book_path(@book.id)
     else
      render :edit
     end
-    
+
   end
 
   def destroy
