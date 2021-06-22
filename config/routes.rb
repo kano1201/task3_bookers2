88201e8f4d057e2d403914b518162881ee6141de
kano1201 <kano.nakamura3@gmail.com>
2021-06-22 05:24:51 +0000

    bookmodel

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
+   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
